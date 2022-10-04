4f6c9ab325c50496170200196f30ebfdb102ab4a
Alec <alecf82@gmail.com>
2022-10-03 22:16:14 -0700

    Lab 4 Submission
    Added button that launches activity to add a question and answer.
    Added button to leave activity.

---
+++ app/src/main/java/com/example/flashcards/AddCardActivity.kt
@@ -0,0 +1,41 @@
+package com.example.flashcards
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class AddCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_card)
+
+        val cancel_button = findViewById<FloatingActionButton>(R.id.cancel_button)
+
+        cancel_button.setOnClickListener{
+            finish()
+        }
+
+        val questionEditText = findViewById<EditText>(R.id.entered_question)
+        val answerEditText = findViewById<EditText>(R.id.entered_answer)
+
+
+
+        val saveButton = findViewById<FloatingActionButton>(R.id.submit_card_button)
+        saveButton.setOnClickListener{
+            val questionString = questionEditText.text.toString()
+            val answerString = answerEditText.text.toString()
+            
+            val data = Intent()
+            data.putExtra("QUESTION_KEY", questionString)
+            data.putExtra("ANSWER_KEY", answerString)
+
+            setResult(RESULT_OK, data)
+            finish()
+        }
+    }
+    }
--- app/src/main/java/com/example/flashcards/MainActivity.kt
+++ app/src/main/java/com/example/flashcards/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.example.flashcards
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val question = findViewById<View>(R.id.flashcard_question)
-        val answer = findViewById<View>(R.id.flashcard_answer)
-        var add_question = findViewById<View>(R.id.add_question_button)
-
+        val question = findViewById<TextView>(R.id.flashcard_question)
+        val answer = findViewById<TextView>(R.id.flashcard_answer)
+        val add_question_button = findViewById<View>(R.id.add_question_button)
 
         question.setOnClickListener{
             question.visibility = View.INVISIBLE
@@ -24,11 +27,29 @@ class MainActivity : AppCompatActivity() {
             question.visibility = View.VISIBLE
             answer.visibility = View.INVISIBLE
         }
+
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                result ->
+
+            val data: Intent? = result.data
+
+            if(data != null){
+                val question_string = data.getStringExtra("QUESTION_KEY")
+                val answer_string = data.getStringExtra("ANSWER_KEY")
+
+                    question.text = question_string
+                    answer.text = answer_string
+            }
+        }
+
+        add_question_button.setOnClickListener{
+            val i = Intent(this@MainActivity, AddCardActivity::class.java)
+            resultLauncher.launch(i)
+        }
         }
 
 
-
-    }
+}
 
 
 
